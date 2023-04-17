3fb465678ae44bb32387b375b382d1676adb3227
Alisson-Augusto <alissonaugustomachadoherculano@hotmail.com>
2023-04-17 12:38:19 -0300

    configuração básica

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def get_app() -> Flask:
   app = Flask(__name__)
-
+  db.init_app(app)
+  
   from .routes import rotas as rotas_blueprint
   app.register_blueprint(rotas_blueprint)
 
---
+++ config.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
+
+
+class Config:
+    SECRET_KEY = environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+  FLASK_ENV = 'production'
+  DEBUG     = False
+  TESTING   = False
+  SQLALCHEMY_DATABASE_URI = environ.get('PROD_DATABASE_URI')
+
+
+class DevConfig(Config):
+  FLASK_ENV = 'development'
+  DEBUG     = True
+  TESTING   = True
+  SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URI')
